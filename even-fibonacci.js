ba96eb46e15dbb6ab2640d62b5dbf1fbd10b6bcf
Greg Turmel <gregturmel@gmail.com>
2016-05-10 17:30:20 -0400

    add cleaner solution to even-fibonnaci

--- even-fibonacci.js
+++ even-fibonacci.js
@@ -19,3 +19,24 @@ for(num = 11; num = evenFibonacci.length; num--) {
 };
 
 console.log(sumFib);
+
+// for a much simpler solution, this is better:
+
+var a = 0;
+var b = 1;
+var c = 0;
+var total = 0;
+
+function evenFib(){
+	while(c < 4000000){
+		c = a + b;
+		a = b;
+		b = c;
+		if(c % 2 === 0){
+			total = c + total;
+		}
+	}
+	return total;
+}
+
+evenFib();
